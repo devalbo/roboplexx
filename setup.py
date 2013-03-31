430e08a15d110c4996d648d1a8aa8d0891eccd2e
Albert Boehmler <ajboehmler@gmail.com>
2013-03-31 00:18:49 -0400

    initial files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='RoboplexxHost',
+    version='0.1dev',
+    packages=['rpxhost',],
+    license='',
+    long_description=open('README.md').read(),
+    requires=[],
+    )
